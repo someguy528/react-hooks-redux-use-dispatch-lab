0f52e59144323732df906fa47478936fe945aaca
Brandon Castro <castroyou999@gmail.com>
2023-03-11 12:17:10 -0500

    completed test

--- src/features/restaurant/Restaurants.js
+++ src/features/restaurant/Restaurants.js
@@ -8,7 +8,7 @@ function Restaurants() {
     <div>
       <ul>
         {restaurants.map((restaurant) => (
-          <li key={restaurant.name}>{restaurant.name}</li>
+          <li key={restaurant.name}>Restaurant name: {restaurant.name}, Location : {restaurant.location}</li>
         ))}
       </ul>
     </div>
